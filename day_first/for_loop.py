eec77bab729361719ab3dc295767dc688b8f9e4a
Sohail ahmad <sohailarman030837@gmail.com>
2025-08-25 11:21:11 +0500

    Add files via upload

---
+++ day_first/for_loop.py
@@ -0,0 +1,16 @@
+
+#this will print number  from one to four
+
+for i in range(1,  5):
+    print(i)
+
+
+
+# for i in range(1, 4):
+#     match i:
+#         case 1:
+#             print("It's the 1st iteration.")
+#         case 2:
+#             print("It's the 2nd iteration.")
+#         case 3:
+#             print("It's the 3rd iteration.")
